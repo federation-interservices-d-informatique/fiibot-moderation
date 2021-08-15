d15bd52e829af8bf7181cb9cf5bb828ac2eb4098
Woomymy <github@woomy.ovh>
2021-08-15 18:18:24 +0200

    Client: Add basic raidmode

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import { fiiClient } from "@federation-interservices-d-informatique/fiibot-common";
+import { GuildMember } from "discord.js";
 import { getDirname } from "./utils/getdirname.js";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const client = new fiiClient(
     {
-        intents: ["GUILDS"]
+        intents: ["GUILDS", "GUILD_MEMBERS"]
     },
     {
         commandManagerSettings: {
@@ -12,5 +13,41 @@ const client = new fiiClient(
         },
         owners: [743851266635071710],
         token: process.env.BOT_TOKEN
+    }
+);
+
+client.eventManager.registerEvent(
+    "checkraidmode",
+    "guildMemberAdd",
+    async (member: GuildMember) => {
+        if (
+            client.commandManager.commands.get("raidmode").data.get("raidmode")
+        ) {
+            try {
+                await member.user.send({
+                    embeds: [
+                        {
+                            title: `Vous avez été expulsé(e) de ${member.guild.name}`,
+                            description:
+                                "Le serveur se trouve actuellement en mode raid, merci de réessayer plus tard.",
+                            color: "RED"
+                        }
+                    ]
+                });
+            } catch (e) {
+                client.logger.error(
+                    `Unable to DM ${member.guild.name}`,
+                    "RAIDMODE"
+                );
+            }
+            try {
+                await member.kick("RAIDMODE");
+            } catch (e) {
+                client.logger.error(
+                    `Can't kick ${member.user.username}`,
+                    "RAIDMODE"
+                );
+            }
+        }
     }
 );
