148c90ca1e703295ad4f949e01736b8fec0dc5cb
Woomy <woomy@woomy.be>
2021-09-26 11:19:17 +0200

    Dependencies: Update FIIBot-common

--- src/index.ts
+++ src/index.ts
@@ -15,10 +15,12 @@ const client = new fiiClient(
         token: process.env.BOT_TOKEN
     },
     {
-        host: process.env.DB_HOST,
-        database: process.env.POSTGRES_DB,
-        password: process.env.POSTGRES_PASSWORD,
-        user: process.env.POSTGRES_USER
+        dbConfig: {
+            host: process.env.DB_HOST,
+            database: process.env.POSTGRES_DB,
+            password: process.env.POSTGRES_PASSWORD,
+            user: process.env.POSTGRES_USER
+        }
     }
 );
 
