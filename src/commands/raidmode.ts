7cd7f3872ab4f0eeb04f4ebfc95509c1b2196065
Woomy <woomy@woomy.be>
2021-09-29 13:36:27 +0200

    Raidmode: Add raidmode logs

--- src/commands/raidmode.ts
+++ src/commands/raidmode.ts
@@ -1,4 +1,4 @@
-import { CommandInteraction } from "discord.js";
+import { CommandInteraction, WebhookClient } from "discord.js";
 import {
     fiiClient,
     Command
@@ -32,9 +32,39 @@ export default class PingCommand extends Command {
         if (inter.options.getSubcommand() === "enable") {
             inter.reply("Le raidmode a été activé!");
             this.data.set("raidmode", true);
+            try {
+                const raidModeHook = new WebhookClient({
+                    id: process.env.RAIDMODE_HOOK_ID,
+                    token: process.env.RAIDMODE_HOOK_TOKEN
+                });
+
+                await raidModeHook.send({
+                    content: `Le raimode a été activé par ${inter.user.tag} (${inter.user.id}) sur ${inter.guildId}`
+                });
+            } catch (e) {
+                this.client.logger.error(
+                    `Can't sent raidmode logs: ${e}`,
+                    "RAIMODE"
+                );
+            }
         } else if (inter.options.getSubcommand() === "disable") {
             inter.reply("Le raidmode a été désactivé");
             this.data.set("raidmode", false);
+            try {
+                const raidModeHook = new WebhookClient({
+                    id: process.env.RAIDMODE_HOOK_ID,
+                    token: process.env.RAIDMODE_HOOK_TOKEN
+                });
+
+                await raidModeHook.send({
+                    content: `Le raimode a été désactivé par ${inter.user.tag} (${inter.user.id}) sur ${inter.guildId}`
+                });
+            } catch (e) {
+                this.client.logger.error(
+                    `Can't sent raidmode logs: ${e}`,
+                    "RAIMODE"
+                );
+            }
         }
     }
 }
