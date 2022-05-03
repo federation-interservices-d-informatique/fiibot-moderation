0e9cc380ba8e9cc8df0e9089db87fd220c5bcde2
Woomymy <github@woomy.ovh>
2022-05-03 20:43:37 +0200

    Docker: Refactor dockerfiles and move to /docker
    * Update developpement redis to 7.0.0-alpine
    * Update developpement postgres to 14.2-alpine
    * Add script to wait for postgres

---
+++ docker/wait-for-postgres.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+import { Client } from "ts-postgres";
+
+const sleep = async (time) => {
+    return new Promise((resolve) => setTimeout(resolve, time));
+};
+
+(async () => {
+    const client = new Client({
+        host: process.env.DB_HOST,
+        user: process.env.POSTGRES_USER,
+        port: parseInt(process.env.POSTGRES_PORT) ?? 5432,
+        database: process.env.POSTGRES_DB,
+        password: process.env.POSTGRES_PASSWORD
+    });
+    console.log("Waiting for postgresql");
+    let error = false;
+    do {
+        try {
+            error = false;
+            await client.connect();
+        } catch (e) {
+            console.log("Failed to connect... retrying");
+            error = true;
+            await sleep(2500);
+        }
+    } while (error);
+    console.log("Conected!");
+
+    process.exit(0);
+})();
