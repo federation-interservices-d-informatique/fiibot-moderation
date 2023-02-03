efd029cc5336c1f9a9464dee754d282dc4e56fbf
Woomy <woomy@woomy.be>
2023-02-03 10:41:08 +0100

    moderation: Use common SERVERS_LIST constant
    Signed-off-by: Woomymy <woomy@woomy.be>

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,12 +1,3 @@
 import { Invite } from "discord.js";
 
 export const INVITATION_REGEX = Invite.InvitesPattern;
-
-export const SERVERS_LIST = [
-    "706283053160464395",
-    "696156347946762240",
-    "456901919344951298",
-    "622831434427662346",
-    "745664551831339150",
-    "877489551206125588"
-];
